3a7cab2adf8943bdf541d4e91e1cf0d35f527762
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-11 15:45:31 -0700

    Selenium grid POC

---
+++ tests/watir-example.rb
@@ -0,0 +1,48 @@
+# http://watir.com/guides/form-example/
+
+require 'watir'
+require 'webdrivers'
+require 'faker'
+
+
+module Driver
+    def self.create(browser)
+      args = ['--ignore-certificate-errors', '--no-sandbox', '--disable-dev-shm-usage']
+
+      @driver = Watir::Browser.new browser.to_sym,
+                                   url: 'http://localhost:4444/wd/hub',
+                                   options: { args: args },
+                                   headless: true
+    end
+
+    def self.get
+      @driver
+    end
+end
+
+
+def findAddressBook(browser_choice)
+
+    # Initalize the Browser
+    browser = Driver.create(browser_choice.to_sym)
+
+    # Resize browser                              
+    browser.window.resize_to(1750, 1000)
+
+    # Navigate to Page
+    browser.goto 'a.testaddressbook.com'
+
+    # Authenticate and Navigate to the Form
+    browser.link(id: 'sign-in').click
+    browser.text_field(data_test: 'email').set 'watir_example@example.com'
+    browser.text_field(data_test: 'password').set 'password'
+    browser.button(name: 'commit').click
+
+    # browser.link(data_test: 'i-do-not-exist').click
+    
+    puts browser_choice.to_s + ' is done!'
+    browser.close
+end
+
+findAddressBook 'chrome'
+findAddressBook 'firefox'
---
+++ tests/webdriver-example.js
@@ -0,0 +1,24 @@
+const webdriver = require("selenium-webdriver");
+
+function findGoogle(browser = "chrome") {
+  const driver = new webdriver.Builder() //Creating a driver
+    .forBrowser(browser)
+    .usingServer("http://localhost:4444/wd/hub") //Register the hub
+    .build();
+
+  driver
+    .get("http://www.google.com") //Opening Google.com
+    .then(() => driver.findElement(webdriver.By.name("q")).sendKeys("hello\n"))
+    // .then(() => driver.findElement(webdriver.By.name("this-should-not-exist")))
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      console.log(`${browser} is done!`);
+      driver.quit();
+    }); //Quit the driver
+}
+
+findGoogle("firefox");
+findGoogle("chrome");
+// findGoogle("edge");
