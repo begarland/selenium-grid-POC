e943ded8fe09bd88ec2902e564c21c570e37b67a
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-12 08:56:11 -0700

    download docker using ubuntu bionic

--- actions/selenium-grid-action/entrypoint.sh
+++ actions/selenium-grid-action/entrypoint.sh
@@ -2,16 +2,10 @@
 
 echo "downloading things"
 
-apt remove docker docker-engine docker.io
-apt install apt-transport-https ca-certificates curl software-properties-common gnupg
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt update
-apt install docker-ce
+apt install docker.io
+systemctl start docker
+systemctl enable docker
+docker --version
 
 echo "Starting swarm..."
 
