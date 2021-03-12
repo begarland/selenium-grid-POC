3d97fb98010708c15554edbffecfa7995418dfe6
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-12 08:41:57 -0700

    download docker with other steps

--- actions/selenium-grid-action/entrypoint.sh
+++ actions/selenium-grid-action/entrypoint.sh
@@ -2,21 +2,16 @@
 
 echo "downloading things"
 
-apt-get update
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+apt remove docker docker-engine docker.io
+apt install apt-transport-https ca-certificates curl software-properties-common gnupg
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt update
+apt install docker-ce
 
 echo "Starting swarm..."
 
