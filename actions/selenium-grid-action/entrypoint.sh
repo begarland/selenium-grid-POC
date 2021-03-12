c2170ddaea13ef3fd45a0a9dd0bd59b9b7591493
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-12 08:23:45 -0700

    download docker a different way

--- actions/selenium-grid-action/entrypoint.sh
+++ actions/selenium-grid-action/entrypoint.sh
@@ -10,7 +10,9 @@ apt-get install \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
