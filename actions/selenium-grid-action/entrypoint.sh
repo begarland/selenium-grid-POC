b46f44d9f20ff2c6affe1a241c5c244897611af3
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-12 08:32:29 -0700

    remove sudo

--- actions/selenium-grid-action/entrypoint.sh
+++ actions/selenium-grid-action/entrypoint.sh
@@ -12,7 +12,7 @@ apt-get install \
 
 
 apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu eoan stable"
 
 echo "Starting swarm..."
