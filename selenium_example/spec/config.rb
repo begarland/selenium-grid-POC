4e202a634e2a82e2769620950d6c3f43a32d1406
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-18 14:14:54 -0600

    selenium_guide: chapter 12

--- selenium_example/spec/config.rb
+++ selenium_example/spec/config.rb
@@ -1,6 +1,9 @@
 # filename: config.rb
 module Config 
     def config
-        { base_url: ENV['BASE_URL'] || 'http://the-internet.herokuapp.com' } 
+        { 
+            base_url: ENV['BASE_URL'] || 'http://the-internet.herokuapp.com',
+            browser_name: ENV['BROWSER_NAME'] || 'chrome'
+        } 
     end
 end
