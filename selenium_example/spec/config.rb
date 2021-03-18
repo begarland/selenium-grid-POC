1d80a97c1115e8d99fa036c73106922ee2e37274
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-18 14:49:57 -0600

    selenium_guide chapter 13

--- selenium_example/spec/config.rb
+++ selenium_example/spec/config.rb
@@ -1,9 +1,18 @@
 # filename: config.rb
+require 'dotenv'
+Dotenv.load('.env')
+
+
 module Config 
     def config
         { 
             base_url: ENV['BASE_URL'] || 'http://the-internet.herokuapp.com',
-            browser_name: ENV['BROWSER_NAME'] || 'chrome'
+            host: ENV['HOST'] || 'saucelabs',
+            browser_name: ENV['BROWSER_NAME'] || 'internet_explorer',
+            browser_version: ENV['BROWSER_VERSION'] || '11',
+            platform_name: ENV['PLATFORM_NAME'] || 'Windows 10',
+            sauce_username: ENV['SAUCE_USERNAME'],
+            sauce_access_key: ENV['SAUCE_ACCESS_KEY']
         } 
     end
 end
--- selenium_example/spec/spec_helper.rb
+++ selenium_example/spec/spec_helper.rb
@@ -3,10 +3,23 @@
 require 'selenium-webdriver'
 require_relative 'config'
 
+
 RSpec.configure do |c| 
   include Config
   
+  
   c.before do |example|
+  case config[:host] 
+  when 'saucelabs'
+    caps = Selenium::WebDriver::Remote::Capabilities.send(config[:browser_name]) 
+    caps[:browser_version] = config[:browser_version]
+    caps[:platform_name] = config[:platform_name]
+    url = "https://#{config[:sauce_username]}:#{config[:sauce_access_key]}@ondemand.saucelabs.com/wd/hub"
+    @driver = Selenium::WebDriver.for(
+            :remote,
+            url: url,
+            desired_capabilities: caps)
+  when 'localhost'
     case config[:browser_name] 
       when 'firefox'
         driver_path = File.join(Dir.pwd, 'vendor', 'geckodriver') 
@@ -19,14 +32,28 @@ RSpec.configure do |c|
           service = Selenium::WebDriver::Service.chrome(path: driver_path) 
         end
       end
-  if service
-    @driver = Selenium::WebDriver.for config[:browser_name].to_sym, service: service 
-  else
-    @driver = Selenium::WebDriver.for config[:browser_name].to_sym end
+      if service
+        @driver = Selenium::WebDriver.for config[:browser_name].to_sym, service: service
+      else
+        @driver = Selenium::WebDriver.for config[:browser_name].to_sym 
+      end
+    end 
   end
 
-  config.after do |example| 
-    @driver.quit
+  
+  c.after do |example| 
+    begin
+      if config[:host] == 'saucelabs'
+        test_passed = example.exception.nil? 
+        @driver.execute_script("sauce:job-name=#{example.full_description}") 
+        @driver.execute_script("sauce:job-result=#{test_passed}")
+      if !test_passed
+        puts "Watch a video of the test at https://saucelabs.com/tests/#{@driver. session_id}"
+      end 
+    end
+    ensure
+      @driver.quit 
+    end
   end
 
 end
