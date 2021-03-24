48b78931011c042ba6ed71c5b2a9887530ec0604
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-24 16:16:57 -0600

    wait on sg

--- actions/selenium-grid-action/entrypoint.sh
+++ actions/selenium-grid-action/entrypoint.sh
@@ -46,15 +46,7 @@ cd tests/
 
 npm install
 
-set -e
 
-while ! curl -sSL "http://172.17.0.2:4444/wd/hub/status" 2>&1 \
-        | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
-    echo 'Waiting for the Grid'
-    sleep 1
-done
-
->&2 echo "Selenium Grid is up - executing tests"
 node webdriver-example.js
 ruby watir-example.rb 
 
