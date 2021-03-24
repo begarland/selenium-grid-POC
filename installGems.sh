2f34acbd460864c3466c39047af0d97f7970c169
Brandy Garland <brandy.e.garland@gmail.com>
2021-03-24 14:47:20 -0600

    log containers

--- installGems.sh
+++ installGems.sh
@@ -3,6 +3,9 @@
 sudo apt-get update
 sudo apt-get install ruby-full
 
+export PATH="/home/runner/.gem/ruby/2.7.0/bin:$PATH"
+
+
 cd $GITHUB_WORKSPACE/tests/
 
 gem install watir webdrivers faker --user-install
