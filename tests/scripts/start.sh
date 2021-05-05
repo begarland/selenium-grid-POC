509455b7cefafbac9d3633ca1a83e51a1825f86c
begarland <brandy.e.garland@gmail.com>
2021-05-05 15:04:10 -0600

    try removing hub config

--- tests/scripts/start.sh
+++ tests/scripts/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-java -jar selenium-server-standalone.jar -hubConfig hub.json
+java -jar selenium-server-standalone.jar
 java -jar -Dwebdriver.gecko.driver=./drivers/geckodriver -Dwebdriver.chrome.driver=./drivers/chromedriver selenium-server-standalone.jar -role node -nodeConfig node.json
