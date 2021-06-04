fb3684ddd77524b5c0137038f5d75d42e6c0cccd
Brandy Garland <brandy.e.garland@gmail.com>
2021-06-04 12:11:04 -0600

    remove safari for now

--- scripts/processTestOutput.sh
+++ scripts/processTestOutput.sh
@@ -6,8 +6,8 @@ cd $GITHUB_WORKSPACE/tests
 
 cat ./artifacts/chrome.ndjson | ./cucumber-json-formatter --format ndjson  > ./artifacts/chrome.json
 cat ./artifacts/firefox.ndjson | ./cucumber-json-formatter --format ndjson  > ./artifacts/firefox.json
-cat ./artifacts/safari.ndjson | ./cucumber-json-formatter --format ndjson  > ./artifacts/safari.json
+# cat ./artifacts/safari.ndjson | ./cucumber-json-formatter --format ndjson  > ./artifacts/safari.json
 
 report_builder -s artifacts/chrome.json -o artifacts/chrome_report
 report_builder -s artifacts/firefox.json -o artifacts/firefox_report
-report_builder -s artifacts/safari.json -o artifacts/safari_report
+# report_builder -s artifacts/safari.json -o artifacts/safari_report
--- scripts/runRuby.sh
+++ scripts/runRuby.sh
@@ -8,4 +8,4 @@ chmod +x ./cucumber-json-formatter
 
 bundle exec cucumber --format message -o ./artifacts/chrome.ndjson BROWSER=chrome 
 bundle exec cucumber --format message -o ./artifacts/firefox.ndjson BROWSER=firefox 
-bundle exec cucumber --format message -o ./artifacts/safari.ndjson BROWSER=safari 
+# bundle exec cucumber --format message -o ./artifacts/safari.ndjson BROWSER=safari  
