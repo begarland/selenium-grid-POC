27f6cc4aea2eff7b83dd16d4885c9790f1194356
Brandy Garland <brandy.e.garland@gmail.com>
2021-06-04 11:52:26 -0600

    add cucumber-json-standalone and  report_builder

--- scripts/runRuby.sh
+++ scripts/runRuby.sh
@@ -4,6 +4,19 @@ cd $GITHUB_WORKSPACE/tests
 
 bundle install 
 
-bundle exec cucumber --publish BROWSER=chrome 
-bundle exec cucumber --publish BROWSER=firefox 
-bundle exec cucumber --publish BROWSER=safari 
+chmod +x ./cucumber-json-formatter
+
+bundle exec cucumber --format message -o ./artifacts/chrome.ndjson BROWSER=chrome 
+bundle exec cucumber --format message -o ./artifacts/firefox.ndjson BROWSER=firefox 
+bundle exec cucumber --format message -o ./artifacts/safari.ndjson BROWSER=safari 
+
+cat ./artifacts/chrome.ndjson | ./cucumber-json-formatter --foramt ndjson  > ./artifacts/chrome.json
+cat ./artifacts/firefox.ndjson | ./cucumber-json-formatter --foramt ndjson  > ./artifacts/firefox.json
+cat ./artifacts/safari.ndjson | ./cucumber-json-formatter --foramt ndjson  > ./artifacts/safari.json
+
+
+report_builder -s artifacts/chrome.json -o artifacts/chrome_report
+report_builder -s artifacts/firefox.json -o artifacts/firefox_report
+report_builder -s artifacts/safari.json -o artifacts/safari_report
+
+
