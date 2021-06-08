d6dde99f9bca7ab475b650bf96e193775a45622e
Brandy Garland <brandy.e.garland@gmail.com>
2021-06-07 18:51:19 -0600

    try with retry and format progress

--- scripts/runRuby.sh
+++ scripts/runRuby.sh
@@ -6,6 +6,6 @@ bundle install
 
 chmod +x ./cucumber-json-formatter
 
-bundle exec cucumber --publish --format message -o ./artifacts/chrome.ndjson BROWSER=chrome 
-bundle exec cucumber --publish --format message -o ./artifacts/firefox.ndjson BROWSER=firefox 
+bundle exec cucumber --retry 2 -f progress --publish --format message -o ./artifacts/chrome.ndjson BROWSER=chrome 
+bundle exec cucumber --retry 2 -f progress --publish --format message -o ./artifacts/firefox.ndjson BROWSER=firefox 
 # bundle exec cucumber --format message -o ./artifacts/safari.ndjson BROWSER=safari  
