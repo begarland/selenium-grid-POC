8c5133e82a939126e4569bc2605a61121f2928ea
Brandy Garland <brandy.e.garland@gmail.com>
2021-06-07 18:43:35 -0600

    test with using publish command

--- scripts/runRuby.sh
+++ scripts/runRuby.sh
@@ -6,6 +6,6 @@ bundle install
 
 chmod +x ./cucumber-json-formatter
 
-bundle exec cucumber --format message -o ./artifacts/chrome.ndjson BROWSER=chrome 
-bundle exec cucumber --format message -o ./artifacts/firefox.ndjson BROWSER=firefox 
+bundle exec cucumber --publish --format message -o ./artifacts/chrome.ndjson BROWSER=chrome 
+bundle exec cucumber --publish --format message -o ./artifacts/firefox.ndjson BROWSER=firefox 
 # bundle exec cucumber --format message -o ./artifacts/safari.ndjson BROWSER=safari  
