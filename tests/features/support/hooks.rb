7b0bb8e2480e215635a71d284736b32276621fa4
Brandy Garland <brandy.e.garland@gmail.com>
2021-06-14 10:54:28 -0600

    add reporting for tests

--- tests/features/support/hooks.rb
+++ tests/features/support/hooks.rb
@@ -4,12 +4,19 @@ Before do |scenario|
     # Resize browser        
     Watir.default_timeout = 90        
     @browser.window.resize_to(1750, 1000)
-    
+    @browser.execute_script("sauce:job-name=#{scenario.name}") 
+
 end
 
 
 After do |scenario|
     log(browser_name.to_s)
     log('done!')
+
+    log(scenario.name)
+
+    
+    test_passed = scenario.passed?
+    @browser.execute_script("sauce:job-result=#{test_passed}")
     @browser.close
 end
