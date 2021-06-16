206bbab72ad2de0029539a0e3db60639a757c831
Brandy Garland <brandy.e.garland@gmail.com>
2021-06-16 13:38:54 -0600

    try with bash

--- scripts/rubyBundleInstall.sh
+++ scripts/rubyBundleInstall.sh
@@ -6,4 +6,5 @@ sudo gem install bundler
 
 bundle install 
 
-bundle exec cucumber
+bundle exec cucumber BROWSER=CHROME PLATFORM='windows 10' BROWSER_VERSION=91.0
+bundle exec cucumber BROWSER=CHROME PLATFORM='macOS 11' BROWSER_VERSION=91.0
