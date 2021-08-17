a7919933268bb7bce5a3cd4ff7904fd1c5d75358
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:25:05 -0600

    use proper drivers

--- scripts/windows_start.sh
+++ scripts/windows_start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 java -jar selenium-server-standalone.jar
-java -jar -Dwebdriver.gecko.driver=.\drivers\geckodriver -Dwebdriver.chrome.driver=.\drivers\chromedriver selenium-server-standalone.jar -role node -nodeConfig node.json
+java -jar -Dwebdriver.gecko.driver=.\drivers\geckodriver.exe -Dwebdriver.chrome.driver=.\drivers\chromedriver.exe selenium-server-standalone.jar -role node -nodeConfig node.json
