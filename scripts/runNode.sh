d7ba3650461601612c939fb20626b31cbac228dc
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:11:50 -0600

    try adding a curl

--- scripts/runNode.sh
+++ scripts/runNode.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+curl http://localhost:4444/wd/hub
+
 cd $GITHUB_WORKSPACE/tests
 
 npm install
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,3 @@
 
 java -jar selenium-server-standalone.jar
 java -jar -Dwebdriver.gecko.driver=./drivers/geckodriver -Dwebdriver.chrome.driver=./drivers/chromedriver selenium-server-standalone.jar -role node -nodeConfig node.json
-
-webdriver-manager status
-webdriver-manager start --standalone
-webdriver-manager status
