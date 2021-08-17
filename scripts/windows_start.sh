ccd678d64f70c44551ff717f4cad0f357a54dd08
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:20:30 -0600

    windows specific script

---
+++ scripts/windows_start.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+java -jar selenium-server-standalone.jar
+java -jar -Dwebdriver.gecko.driver=.\drivers\geckodriver -Dwebdriver.chrome.driver=.\drivers\chromedriver selenium-server-standalone.jar -role node -nodeConfig node.json
