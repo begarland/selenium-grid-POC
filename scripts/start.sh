b3af70377f856f0c2be889d7d1510571436af2a1
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:07:38 -0600

    try getting status

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,6 @@
 java -jar selenium-server-standalone.jar
 java -jar -Dwebdriver.gecko.driver=./drivers/geckodriver -Dwebdriver.chrome.driver=./drivers/chromedriver selenium-server-standalone.jar -role node -nodeConfig node.json
 
+webdriver-manager status
 webdriver-manager start --standalone
+webdriver-manager status
