f1625fc4da8090a11b39fb7acc9e3ae2cd23a52b
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:28:11 -0600

    try with curling in the file

--- scripts/windows_start.sh
+++ scripts/windows_start.sh
@@ -2,3 +2,8 @@
 
 java -jar selenium-server-standalone.jar
 java -jar -Dwebdriver.gecko.driver=.\drivers\geckodriver.exe -Dwebdriver.chrome.driver=.\drivers\chromedriver.exe selenium-server-standalone.jar -role node -nodeConfig node.json
+
+curl http://localhost:4444/wd/hub
+curl http://127.0.0.1:4444/wd/hub
+
+echo "whyyy aren't you workingggg"
