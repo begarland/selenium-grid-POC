af68086a3d94afcd797601de8603c6f9e4b37790
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:01:17 -0600

    try with standalone command

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,3 +2,5 @@
 
 java -jar selenium-server-standalone.jar
 java -jar -Dwebdriver.gecko.driver=./drivers/geckodriver -Dwebdriver.chrome.driver=./drivers/chromedriver selenium-server-standalone.jar -role node -nodeConfig node.json
+
+webdriver-manager start --standalone
