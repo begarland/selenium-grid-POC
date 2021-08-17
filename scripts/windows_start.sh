ccc1fb59fa6d0fbb154ebb82ee0e5f5488c92fda
begarland <brandy.e.garland@gmail.com>
2021-08-17 14:47:19 -0600

    please work

--- scripts/windows_start.sh
+++ scripts/windows_start.sh
@@ -2,8 +2,3 @@
 
 java -jar selenium-server-standalone.jar
 java -jar -Dwebdriver.gecko.driver=.\drivers\geckodriver.exe -Dwebdriver.chrome.driver=.\drivers\chromedriver.exe selenium-server-standalone.jar -role node -nodeConfig node.json
-
-Write-Output curl http://localhost:4444/wd/hub
-Write-Output curl http://127.0.0.1:4444/wd/hub
-
-Write-Output  "whyyy aren't you workingggg"
