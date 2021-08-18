49ee815f20e4491c4636a15b86b51ea3de7c6cec
begarland <brandy.e.garland@gmail.com>
2021-08-17 20:57:46 -0600

    plzzzz

--- tests/webdriver-example.js
+++ tests/webdriver-example.js
@@ -1,36 +1,38 @@
-const webdriver = require("selenium-webdriver");
+const webdriver = require('selenium-webdriver');
 
-console.log('attempting to run...')
+console.log('attempting to run...');
 
-function findGoogle(browser = "chrome") {
-  console.log('the browser is', browser)
+function findGoogle(browser = 'chrome') {
+  console.log('the browser is', browser);
   if (browser == 'internet explorer') {
-    console.log('hello, this will break')
+    console.log('hello, this will break');
   }
   const driver = new webdriver.Builder() //Creating a driver
     .forBrowser(browser)
-    .usingServer("http://localhost:4444/wd/hub") //Register the hub
+    .usingServer('http://localhost:4444/wd/hub') //Register the hub
     .build();
-    if (browser == 'internet explorer') {
-      console.log('we made it past teh driver, this will break')
-    }
-  driver
-    .get("http://www.google.com") //Opening Google.com
-    .then(() => {driver.findElement(webdriver.By.name("q")).sendKeys("hello\n");       console.log(`${browser} has searched!`);
-  })
-    // .then(() => driver.findElement(webdriver.By.name("this-should-not-exist")))
-    .catch((err) => {
-      // console.log(err)
-      throw Error(err)
-      console.log(`${browser} has errored!`);
-
-    })
-    .finally(() => {
-      console.log(`${browser} is done!`);
-      driver.quit();
-    }); //Quit the driver
+  if (browser !== 'internet explorer') {
+    driver
+      .get('http://www.google.com') //Opening Google.com
+      .then(() => {
+        driver.findElement(webdriver.By.name('q')).sendKeys('hello\n');
+        console.log(`${browser} has searched!`);
+      })
+      // .then(() => driver.findElement(webdriver.By.name("this-should-not-exist")))
+      .catch((err) => {
+        // console.log(err)
+        console.log(`${browser} has errored!`);
+        throw Error(err);
+      })
+      .finally(() => {
+        console.log(`${browser} is done!`);
+        driver.quit();
+      }); //Quit the driver
+  } else {
+    driver.quit();
+  }
 }
 
 module.exports = {
-  findGoogle
-}
+  findGoogle,
+};
